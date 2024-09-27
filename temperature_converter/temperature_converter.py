422d172c85fa40e786068cf45d69a2e3569ee180
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 21:52:33 +0800

    Ask the user to select the convertion

--- temperature_converter/temperature_converter.py
+++ temperature_converter/temperature_converter.py
@@ -4,4 +4,5 @@
 # Ask the user to input a temperature
 temp = float(input("Please enter the temperature without the unit: "))
 # Ask the user to select the type of convertion
+convertion = input("Please select the convertion type.\n(1)Celcius to Fahrenheit \n(2)Fahrenheit to Celcius \n")
 # Convert the input
