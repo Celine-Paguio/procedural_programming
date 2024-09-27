db99ab5ef9e7b1fc9115400aa5a656b665bd1741
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:07:04 +0800

    Ask the user for an input

--- ohm's_law_calculator/ohm's_law_calculator.py
+++ ohm's_law_calculator/ohm's_law_calculator.py
@@ -1,6 +1,10 @@
 # This program will calculate the voltage, current or resistance.
 
+# Start of program
 # Ask for the user to choose what they would like to calculate 
+choice = input("What would you like to calculate? Please enter V for Voltage, C for Current, or R for Resistance): ")
+# Remove the spaces and capitalize the input if in lowercase"
+choice = choice.strip().upper()
 # Ask for the appropriate values
 # Calculate for the missing variable
 # Handle cases where division by zero might occur
