89fc7ec679cb65b580f406487f474796eac4dc20
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:23:30 +0800

    Pseudocode

---
+++ diamond_shape/diamond_shape.py
@@ -0,0 +1,6 @@
+# This program will create Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
+
+# Write the print_diamond function
+# Check if the input is an even integer
+# Ask the user for input
+# Call the function
