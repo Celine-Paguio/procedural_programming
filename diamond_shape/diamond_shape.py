f639a6f7421607288684826b7afbcc53d6915b38
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:27:10 +0800

    Print the top half

--- diamond_shape/diamond_shape.py
+++ diamond_shape/diamond_shape.py
@@ -1,9 +1,15 @@
 # This program will create Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
 
+# Start of program
 # Write the print_diamond function
 def print_diamond(n):
 # Check if the input is an even integer
     if n % 2 == 0:
         return "Please provide an odd integer."
+# Print the top half of the diamond
+    for i in range(n // 2 + 1):
+        spaces = ' ' * (n // 2 - i)
+        asterisk = '*' * (2 * i + 1)
+        print (spaces + asterisk)
 # Ask the user for input
 # Call the function
