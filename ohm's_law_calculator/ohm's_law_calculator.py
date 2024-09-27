09c21a217923bce88787d6efd6fc48bc6f974a83
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:19:38 +0800

    Exception handling

--- ohm's_law_calculator/ohm's_law_calculator.py
+++ ohm's_law_calculator/ohm's_law_calculator.py
@@ -5,28 +5,33 @@
 choice = input("What would you like to calculate? Please enter V for Voltage, C for Current, or R for Resistance): ")
 # Remove the spaces and capitalize the input if in lowercase"
 choice = choice.strip().upper()
-# Calculate voltage
-if choice == "V":
-# Ask for the appropriate values
-    current = float(input("Enter the current in Amperes: "))
-    resistance = float(input("Enter the resistance in Ohms: "))
-# Computation
-    voltage = current * resistance
-    print("The voltage is %.2f" %voltage, "volts.")
-# Calculate current
-elif choice == "C":
-# Ask for the appropriate values
-    voltage = float(input("Enter the voltage in Volts: "))
-    resistance = float(input("Enter the resistance in Ohms: "))
-# Computation
-    current = voltage / resistance
-    print("The current is %.2f" %current, "amperes.")
-# Calculate resistance
-elif choice == "R":
-# Ask for the appropriate values
-    voltage = float(input("Enter the voltage in Volts: "))
-    current = float(input("Enter the current in Amperes: "))
-# Computation
-    resistance = voltage / current
-    print("The resistance is %.2f" %resistance, "ohms.")
 # Handle cases where division by zero might occur
+try: 
+# Calculate voltage
+    if choice == "V":
+# Ask for the appropriate values
+        current = float(input("Enter the current in Amperes: "))
+        resistance = float(input("Enter the resistance in Ohms: "))
+# Computation
+        voltage = current * resistance
+        print("The voltage is %.2f" %voltage, "volts.")
+# Calculate current
+    elif choice == "C":
+# Ask for the appropriate values
+        voltage = float(input("Enter the voltage in Volts: "))
+        resistance = float(input("Enter the resistance in Ohms: "))
+# Computation
+        current = voltage / resistance
+        print("The current is %.2f" %current, "amperes.")
+# Calculate resistance
+    elif choice == "R":
+# Ask for the appropriate values
+        voltage = float(input("Enter the voltage in Volts: "))
+        current = float(input("Enter the current in Amperes: "))
+# Computation
+        resistance = voltage / current
+        print("The resistance is %.2f" %resistance, "ohms.")
+except ZeroDivisionError:
+    print("Sorry! You are dividing by zero.")
+    
+# End of program
