9202b5ae3a50b5ae24a3f48dc0d17b03e13d77fe
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 21:50:56 +0800

    Ask the user for a temperature

--- temperature_converter/temperature_converter.py
+++ temperature_converter/temperature_converter.py
@@ -1,5 +1,7 @@
 # This program that will ask a user to input a temperature and covert it into Celcius or Fahrenheit
 
+# Start of program
 # Ask the user to input a temperature
-# Ask the uesr to select the type of convertion
+temp = float(input("Please enter the temperature without the unit: "))
+# Ask the user to select the type of convertion
 # Convert the input
