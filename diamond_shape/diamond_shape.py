cfd98665430a26a5a4e68eaad4697fc5db34f0d6
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:25:09 +0800

    Check if the input is even

--- diamond_shape/diamond_shape.py
+++ diamond_shape/diamond_shape.py
@@ -1,6 +1,9 @@
 # This program will create Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
 
 # Write the print_diamond function
+def print_diamond(n):
 # Check if the input is an even integer
+    if n % 2 == 0:
+        return "Please provide an odd integer."
 # Ask the user for input
 # Call the function
