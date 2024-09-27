67f9c01219bae13b143b7eaed06139b6bc1d9de7
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 21:48:40 +0800

    Pseudocode

---
+++ temperature_converter/temperature_converter.py
@@ -0,0 +1,5 @@
+# This program that will ask a user to input a temperature and covert it into Celcius or Fahrenheit
+
+# Ask the user to input a temperature
+# Ask the uesr to select the type of convertion
+# Convert the input
