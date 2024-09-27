7ac63b6898f6dbfe15a7011ffc5e29add463956f
Celine Paguio <celinerosespaguio@iskolarngbayan.pup.edu.ph>
2024-09-27 22:04:35 +0800

    Pseudocode

---
+++ ohm's_law_calculator/ohm's_law_calculator.py
@@ -0,0 +1,6 @@
+# This program will calculate the voltage, current or resistance.
+
+# Ask for the user to choose what they would like to calculate 
+# Ask for the appropriate values
+# Calculate for the missing variable
+# Handle cases where division by zero might occur
